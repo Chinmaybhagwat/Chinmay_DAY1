ce226c5b0ebadb657e74569ce775c0af2b278e70
Chinmaybhagwat <35619926+Chinmaybhagwat@users.noreply.github.com>
2021-10-11 18:46:48 +0530

    Add files via upload

---
+++ Assignment_day1/Assign_d1/src/com/basics/Testpoint2D.java
@@ -0,0 +1,11 @@
+package com.basics;
+
+public class Testpoint2D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
---
+++ Assignment_day1/Assign_d1/src/com/basics/point2D.java
@@ -0,0 +1,13 @@
+package com.basics;
+
+public class point2D {
+	
+	private int x,y;
+	
+	public String getDetails()
+	{
+
+		 System.out.println("point Details:"+x+" "+y);
+	}
+
+}
